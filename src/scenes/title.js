ebda0ea8a7bc364e711418929555df199553de4b
jperone <jperone@ucsc.edu>
2023-06-07 19:53:01 -0700

    Added credits button

--- src/scenes/title.js
+++ src/scenes/title.js
@@ -8,6 +8,8 @@ class Title extends Phaser.Scene {
         this.load.image("background", "src/assets/titleMenu/menuBackground.png");
         this.load.image("playButtonBack", "src/assets/titleMenu/PlayButtonBack.png");
         this.load.image("playButtonText", "src/assets/titleMenu/PlayButtonFront.png");
+        this.load.image("creditsButtonBack", "src/assets/titleMenu/CreditsButtonBack.png");
+        this.load.image("creditsButtonText", "src/assets/titleMenu/CreditsButtonText.png");
     }
 
     create() {
@@ -22,17 +24,24 @@ class Title extends Phaser.Scene {
         var style2 = { font: "Bold 42px Arial", fill: '0x000000', boundsAlignH: 'center', boundsAlignV: 'middle'};
         let text1 = this.add.text(1920/2, 1080/3, "[GAME TITLE]", style).setOrigin(.5,.5);
 
-        let playButtonBack = this.add.image(1920/2, 1080/1.8, 'playButtonBack').setScale(1.5).setInteractive().on('pointerover', () => {
-            playButtonText.setScale(1);
+        let playButtonBack = this.add.image(1920/2, 1080/1.8, 'playButtonBack').setScale(1.6).setInteractive().on('pointerover', () => {
+            playButtonText.setScale(1.3);
         });;
-        let playButtonText = this.add.image(1920/2, 1080/1.8, 'playButtonText').setScale(1.5).setInteractive().on('pointerover', () => {
-            playButtonText.setScale(1);
+        let playButtonText = this.add.image(1920/2, 1080/1.8, 'playButtonText').setScale(1.6).setInteractive().on('pointerover', () => {
+            playButtonText.setScale(1.3);
         });
 
-        let text2 = this.add.text(1920/2, 1080/1.3, "[other stuff]", style2).setOrigin(.5,.5);
+        let creditsButtonBack = this.add.image(1920/2, 1080/1.35, 'creditsButtonBack').setScale(1.25).setInteractive().on('pointerover', () => {
+            creditsButtonText.setScale(1);
+        });
+        let creditsButtonText = this.add.image(1920/2, 1080/1.35, 'creditsButtonText').setScale(1.25).setInteractive().on('pointerover', () => {
+            creditsButtonText.setScale(1);
+        });
+
         
         background.on('pointerover', () => {
-            playButtonText.setScale(1.5);
+            playButtonText.setScale(1.6);
+            creditsButtonText.setScale(1.25);
         });
 
         this.cameras.main.on('camerafadeincomplete', () => {
