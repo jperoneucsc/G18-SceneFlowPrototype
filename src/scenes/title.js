475c09c20de5eae0968112b4558714709488f22d
jperone <jperone@ucsc.edu>
2023-06-07 20:57:27 -0700

    comments

--- src/scenes/title.js
+++ src/scenes/title.js
@@ -14,16 +14,17 @@ class Title extends Phaser.Scene {
     }
 
     create() {
-        this.game.sound.stopAll();
         this.cameras.main.fadeIn(2000);
 
 
         let background = this.add.image(0, 0, 'background').setOrigin(0,0).setInteractive();
 
-
+        // Initialize fonts
         var style = { font: "Bold 132px Arial", fill: '0x000000', boundsAlignH: 'center', boundsAlignV: 'middle'};
         var style2 = { font: "Bold 42px Arial", fill: '0x000000', boundsAlignH: 'center', boundsAlignV: 'middle'};
-        let title = this.add.text(700, 300, "[GAME TITLE]", style).setOrigin(.5,.5);
+
+        let title = this.add.text(700, 300, "[GAME TITLE]", style).setOrigin(.5,.5);       // Title 
+        // Highscore and floating animation
         let highScore = this.add.text(1400, 275, "Your high score: ~", style2).setOrigin(.5,.5).setRotation(.6);
         var tween = this.tweens.add({
             targets: highScore,
@@ -34,6 +35,7 @@ class Title extends Phaser.Scene {
             duration: 1000,
         })
 
+        // Temporary asset in bottom right and rotating animation
         let tempAsset = this.add.plane(1450, 700, 'tempMenuAsset');
         var tween = this.tweens.add({
             targets: tempAsset,
@@ -44,7 +46,7 @@ class Title extends Phaser.Scene {
             duration: 5000,
         })
 
-
+        // Create play button back and text layer
         let playButtonBack = this.add.image(700, 1080/1.8, 'playButtonBack').setScale(1.6).setInteractive().on('pointerover', () => {
             playButtonText.setScale(1.3);
         });;
@@ -72,12 +74,13 @@ class Title extends Phaser.Scene {
             });
         });
 
-        
+        // If the background is being hovered over, reset button scales
         background.on('pointerover', () => {
             playButtonText.setScale(1.6);
             creditsButtonText.setScale(1.25);
         });
 
+        // WIP: When the camera fades in, scale in all of the buttons and stuff
         this.cameras.main.on('camerafadeincomplete', () => {
             
         });
