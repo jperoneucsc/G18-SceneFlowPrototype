ca55a5728e953de4276f5cc9c1105330faf80843
jperone <jperone@ucsc.edu>
2023-06-07 20:32:49 -0700

    added temp asset in menu

--- src/scenes/title.js
+++ src/scenes/title.js
@@ -10,6 +10,7 @@ class Title extends Phaser.Scene {
         this.load.image("playButtonText", "src/assets/titleMenu/PlayButtonFront.png");
         this.load.image("creditsButtonBack", "src/assets/titleMenu/CreditsButtonBack.png");
         this.load.image("creditsButtonText", "src/assets/titleMenu/CreditsButtonText.png");
+        this.load.image("tempMenuAsset", "src/assets/titleMenu/tempMenuAsset.png");
     }
 
     create() {
@@ -22,19 +23,39 @@ class Title extends Phaser.Scene {
 
         var style = { font: "Bold 132px Arial", fill: '0x000000', boundsAlignH: 'center', boundsAlignV: 'middle'};
         var style2 = { font: "Bold 42px Arial", fill: '0x000000', boundsAlignH: 'center', boundsAlignV: 'middle'};
-        let text1 = this.add.text(1920/2, 1080/3, "[GAME TITLE]", style).setOrigin(.5,.5);
+        let title = this.add.text(700, 300, "[GAME TITLE]", style).setOrigin(.5,.5);
+        let highScore = this.add.text(1400, 275, "Your high score: ~", style2).setOrigin(.5,.5).setRotation(.6);
+        var tween = this.tweens.add({
+            targets: highScore,
+            y: { start: 275, from: 280, to: 260},
+            ease:'Power1',
+            yoyo: true,
+            repeat: -1,
+            duration: 1000,
+        })
 
-        let playButtonBack = this.add.image(1920/2, 1080/1.8, 'playButtonBack').setScale(1.6).setInteractive().on('pointerover', () => {
+        let tempAsset = this.add.plane(1450, 700, 'tempMenuAsset');
+        var tween = this.tweens.add({
+            targets: tempAsset,
+            angle: {from: 0, to: 360},
+            ease:'Linear',
+            yoyo: false,
+            repeat: -1,
+            duration: 5000,
+        })
+
+
+        let playButtonBack = this.add.image(700, 1080/1.8, 'playButtonBack').setScale(1.6).setInteractive().on('pointerover', () => {
             playButtonText.setScale(1.3);
         });;
-        let playButtonText = this.add.image(1920/2, 1080/1.8, 'playButtonText').setScale(1.6).setInteractive().on('pointerover', () => {
+        let playButtonText = this.add.image(700, 1080/1.8, 'playButtonText').setScale(1.6).setInteractive().on('pointerover', () => {
             playButtonText.setScale(1.3);
         });
 
-        let creditsButtonBack = this.add.image(1920/2, 1080/1.35, 'creditsButtonBack').setScale(1.25).setInteractive().on('pointerover', () => {
+        let creditsButtonBack = this.add.image(700, 1080/1.35, 'creditsButtonBack').setScale(1.25).setInteractive().on('pointerover', () => {
             creditsButtonText.setScale(1);
         });
-        let creditsButtonText = this.add.image(1920/2, 1080/1.35, 'creditsButtonText').setScale(1.25).setInteractive().on('pointerover', () => {
+        let creditsButtonText = this.add.image(700, 1080/1.35, 'creditsButtonText').setScale(1.25).setInteractive().on('pointerover', () => {
             creditsButtonText.setScale(1);
         });
 
