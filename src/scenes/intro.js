9235625df7b34bf4d670fbeab5f75c16fd9d6f76
jperone <jperone@ucsc.edu>
2023-06-07 21:09:53 -0700

    comments

--- src/scenes/intro.js
+++ src/scenes/intro.js
@@ -13,9 +13,10 @@ class Intro extends Phaser.Scene {
 
         let background = this.add.image(0, 0, 'background').setOrigin(0,0).setInteractive();
 
-
+        // fonts
         var style = { font: "Bold 132px Arial", fill: '0x000000', boundsAlignH: 'center', boundsAlignV: 'middle'};
         var style2 = { font: "Bold 42px Arial", fill: '0x000000', boundsAlignH: 'center', boundsAlignV: 'middle'};
+        // add title and movement anim
         let title = this.add.text(960, 500, "[Intro Cinematic]", style).setOrigin(.5,.5);
         var tween = this.tweens.add({
             targets: title,
